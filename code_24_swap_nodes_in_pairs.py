96dc4cce5fcee030507d17d3d461eeb2b39f9124
mjfox33 <mjfox33@gmail.com>
2022-02-17 17:07:31 -0500

    added a method that swaps the nodes not vals

--- code_24_swap_nodes_in_pairs.py
+++ code_24_swap_nodes_in_pairs.py
@@ -5,6 +5,16 @@ class ListNode:
         self.next = next
 class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
+        sentinel = previous_node = ListNode()
+        previous_node.next = head
+        while previous_node.next and previous_node.next.next:
+            temp = previous_node.next
+            temp2 = temp.next
+            previous_node.next, temp2.next, temp.next = temp2 , temp, temp2.next
+            previous_node = temp
+        return sentinel.next
+
+    def swapPairs2(self, head: ListNode) -> ListNode:
         sentinel = ListNode(0, head)
         while head and head.next:
             head.val, head.next.val = head.next.val, head.val
