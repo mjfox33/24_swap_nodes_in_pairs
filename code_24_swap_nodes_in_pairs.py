49b083890011a5a5190e544d9d33d7a3d69281cc
mjfox33 <mjfox33@gmail.com>
2022-02-17 16:50:16 -0500

    initial commit

---
+++ code_24_swap_nodes_in_pairs.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        sentinel = ListNode(0, head)
+        while head and head.next:
+            head.val, head.next.val = head.next.val, head.val
+            head = head.next.next
+        return sentinel.next
